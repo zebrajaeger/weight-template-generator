84c572364a18f2eb6f19611410b596e99b4c3e7f
Lars Brandt <brandt@silpion.de>
2021-02-21 10:36:39 +0100

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# weight-template-generator
+
+This creates a graph to print out on a DIN A4 paper.
+
+![screenshot](2021-02-21_10-31-35.png)
+
+## Motivation
+
+1. Playing al little bit with embedded SVG.
+2. Control my weight during lockdown in a simple way...
+
+## Usage 
+
+
+
+## Develop / Run
+
+NodeJs is required (https://nodejs.org/)
+
+run
+
+    npm install
+
+then run
+
+    npm run serve
+
+and open a Browser of your choice @ http://localhost:3000
+
+BrowserSync watches file changes in 'src' directory and reload the browser content. 
---
+++ src/app.js
@@ -0,0 +1,98 @@
+// DIN-A4 horizontal
+const y1 = 20;
+const y2 = 190;
+const x1 = 50;
+const x2 = 297 - 20;
+
+/**
+ * right aligned text
+ */
+function createRText(x, y, text) {
+    return $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+        .attr({x, y, 'text-anchor': 'end'})
+        .append(document.createTextNode(text));
+}
+
+/**
+ * centered text
+ */
+function createMText(x, y, text) {
+    return $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+        .attr({x, y, 'text-anchor': 'middle '})
+        .append(document.createTextNode(text));
+}
+
+function createLine(x1, y1, x2, y2, clazz) {
+    return $(document.createElementNS('http://www.w3.org/2000/svg', 'line'))
+        .attr({x1, x2, y1, y2, class: clazz});
+}
+
+function createLineRel(x1, y1, w, l, clazz) {
+    return createLine(x1, y1, x1 + w, y1 + l, clazz)
+}
+
+function createYScale(startValue, step, x) {
+    const $g = $('#graph');
+
+    const ySize = (y2 - 8) - y1;
+    const yStep = ySize / 50;
+    $g.append(createLine(x + 2, y1, x + 2, (y2 - 8)));
+
+    let v = startValue;
+    for (let i = 0; i <= 50; ++i) {
+        const y = y1 + (yStep * (50 - i));
+        $g.append(createLineRel(x, y, 2, 0, 'minor'));
+        $g.append(createRText(x - 1, y + 0.75, v.toFixed(1).toString()))
+        v += step;
+    }
+}
+
+function createGrid() {
+    const xStart = 50;
+    const $g = $('#graph');
+    const ySteps = Math.ceil(10 / 0.2);
+    const ySize = (y2 - 8) - y1;
+    const yStep = ySize / ySteps;
+    for (let i = 0; i <= ySteps; ++i) {
+        const y = y1 + (yStep * (ySteps - i));
+        $g.append(createLine(50, y, x2, y, (i % 5) === 0 ? 'major' : 'minor'));
+    }
+
+    const xSize = x2 - xStart;
+    const xStep = xSize / 31;
+    for (let i = 0; i <= 31; ++i) {
+        const x = xStart + (i * xStep);
+        $g.append(createLine(x, y1, x, (y2 - 8), (i === 0 | i === 31) ? 'major' : 'minor'));
+
+        $g.append(createLine(x, y2 - 5, x, y2 - 3, 'minor'));
+        $g.append(createMText(x, y2, i.toString()));
+    }
+
+    $g.append(createLine(50, y2 - 5, x2, y2 - 5, 'minor'));
+}
+
+function readUrlParamFloat(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has(name)) {
+        try {
+            return parseFloat(urlParams.get(name));
+        } catch (e) {
+            console.error(`could not read URL-param '${name}'`)
+        }
+    }
+    return null;
+}
+
+$(() => {
+    $('.mdc-text-field').each((i, el) => new mdc.textField.MDCTextField(el));
+
+    $('.w-start input').val(readUrlParamFloat('wStart') || 75);
+    $('.w-step input').val(readUrlParamFloat('wStep') || 0.2);
+    $('.f-start input').val(readUrlParamFloat('fStart') || 20);
+    $('.f-step input').val(readUrlParamFloat('fStep') || 0.2);
+
+    createYScale(readUrlParamFloat('wStart') || 90, readUrlParamFloat('wStep') || 0.2, 32);
+    createYScale(readUrlParamFloat('fStart') || 25, readUrlParamFloat('fStep') || 0.1, 45);
+    createGrid();
+})
+
